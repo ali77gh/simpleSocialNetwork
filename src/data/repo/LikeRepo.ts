6ab77c359e3bda5500ee546dcb749ca8c181c895
ali <alighahremani1377@gmail.com>
2020-04-15 11:50:19 +0430

    LikeRepo Done

--- src/data/repo/LikeRepo.ts
+++ src/data/repo/LikeRepo.ts
@@ -1,4 +1,5 @@
 import DBTools from "../DBTools";
+import Like from "../model/Like";
 
 
 export default class LikeRepo {
@@ -30,11 +31,45 @@ export default class LikeRepo {
     private static stm = {
 
         insert: undefined,
+        delete: undefined,
+        getLikesByPostOffset: undefined,
+        countLikesByPost:undefined,
     }
 
     private static initStatments() {
         //generate binaries
         this.stm.insert = this.db.prepare(`INSERT INTO ${this.tableName} (who, postId) VALUES (?, ?)`);
+        this.stm.delete = this.db.prepare(`DELETE FROM ${this.tableName} WHERE who=? AND postId=?`);
+        this.stm.getLikesByPostOffset = this.db.prepare(`SELECT who FROM ${this.tableName} WHERE postId=? LIMIT 10 OFFSET ?`);
+        this.stm.countLikesByPost = this.db.prepare(`SELECT count(postId) FROM ${this.tableName} WHERE postId=?`);
         
+    }
+
+    public static newLike(like: Like, fininshed:(err)=>void) {
+        this.stm.insert.run([like.who, like.postID], (err: string) => {
+            fininshed(err)
+        })
+    }
+
+    public static deleteLike(like: Like, fininshed: (err) => void) {
+        this.stm.delete.run([like.who, like.postID], (err: string) => {
+            fininshed(err)
+        })
+    }
+
+    public static getLikesByPostOffset(postId:string,offset:number, fininshed: (err,usernames:string[]) => void) {
+        this.stm.delete.all([postId, offset], (err: string, rows) => {
+            
+            let usernames: string[] = []
+            for (let i of rows)
+                usernames.push(i.who)
+            fininshed(err,usernames)
+        })
+    }
+
+    public static countLikesByPost(postId: string, fininshed: (err,count:number) => void) {
+        this.stm.delete.get([postId], (err: string,row) => {
+            fininshed(err, row["count(postId)"])
+        })
     }
 }
